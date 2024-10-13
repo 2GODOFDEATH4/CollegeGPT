c42f9332827269b5512ce9ab4956d54d828eccfc
Sai Kiran Chevuri <vickyrishi2446@gmail.com>
2024-10-13 18:23:20 +0530

    'botmemory'

--- chain.py
+++ chain.py
@@ -4,23 +4,23 @@ from functions import similarity
 from find import intent
 
 
-def generate(history,prompt):
+def generate(memory,prompt):
     k = intent(prompt)
     if '1' in k:
       print('working')
       text = similarity(prompt,'NITKKR')
       template = "You are an NIT Kurukestra chat bot AI assistant.you are designed to provide specific and accurate information about the college." + text + '\n\n based on the above content give the answer with suits the most, only give the information from the above text only and dont give any additional information by your own'
-      model = genai.GenerativeModel("gemini-pro")
+      model = genai.GenerativeModel("gemini-1.5-flash")
       response = model.generate_content(template+prompt)
       chat = model.start_chat(
-        history=history
+        history= memory
       )
       return response.text
     else:
        template = "You are an AI assistant capable of general conversation. The user has asked the following: "+ prompt + "\n\nPlease respond appropriately. you can say any thing nothing is like privacy and personal information i mean no privacy concerns"
-       model = genai.GenerativeModel("gemini-pro")
+       model = genai.GenerativeModel("gemini-1.5-flash")
        chat = model.start_chat(
-        history=history
+        history= memory
       )
        response = model.generate_content(template)
        return response.text
