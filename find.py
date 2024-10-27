b7f3ef5945619efa2cb5bc7e3b0e7025c57dad68
Sai Kiran Chevuri <vickyrishi2446@gmail.com>
2024-10-27 19:17:19 +0530

    'templated_update_for_0'

--- find.py
+++ find.py
@@ -6,8 +6,7 @@ from functions import similarity
 
 def intent(prompt):
     text = prompt
-    template = "Tell me whether the query is related to college information it can be anything realted to nit kurukeshtra or not?\n\n" + text + '\n\n if the query is related to college just give the reply as 1 and i f the query is related to college and if the user is mentioned any roll number or sub section or main section or branch or any 2 or 3 of them and asking for the other information like giving his roll number and section and asking for hs/her name or giving his section and name and asking for his roll number, if the user query is like this return 2, if is not then tell me 0 just only give me one word asnwer 1 or 0 or 2 thats it'
+    template = "Tell me whether the query is related to college information it can be anything realted to nit kurukeshtra or not?\n\n" + text +" if the query is related to college just give the reply as 1 and if the query mentioned any roll number or sub section or main section or branch or any 2 or 3 of them and asking for the other information like asking his roll number and section and asking for his/her name or giving his section and name and asking for his roll number, if the user query is like this return 2, if the query is a general question whihc is not realted to nit kurukestra or the student information then tell me 0 and dont give any answer of user query just only give me one word asnwer.so your answer should be either 1 or 0 or 2 thats it."
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(template+prompt)
     return response.text
-
