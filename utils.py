a5c3f28910011182805ee4dc938887ff01c02938
Sai Kiran Chevuri <vickyrishi2446@gmail.com>
2026-04-24 22:15:09 +0530

    Fix: Update langchain import for text splitter
    - Changed from langchain.text_splitter to langchain_text_splitters
    - Fixes ModuleNotFoundError in utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def read_doc(directory):
     file_loader = PyPDFDirectoryLoader(directory)
